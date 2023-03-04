333e5915bbc1874a7ccfab5dfb8f62a42bb25641
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-03-03 22:23:10 -0500

    created grid and basic setup. nothing is functional yet. need to research more about looping event listeners to turn the boxes black on mouseover

---
+++ scripts.js
@@ -0,0 +1,26 @@
+
+const main = document.querySelector("#grid")
+
+
+
+function createGrid(size) {
+    for (let i = 0; i < (size * size); i++) {
+        const div = document.createElement("div")
+        main.appendChild(div)
+        div.classList.add("box")
+        const box = document.querySelector(".box")
+        }
+    }
+    
+
+
+
+createGrid(16)
+
+// function fillBox(b) {
+//     var bx = b.target
+//     bx.style.backgroundColor = "black"
+    
+// }
+
+
