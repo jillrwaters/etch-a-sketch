b5cd10a121a466579f8e73ebc842d1c370eca9f5
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-03-06 17:19:07 -0500

    resize button now working EXCEPT FOR 2 THINGS: 1)pressing resize button again causes an increasing portion of the bottom of the grid container to turn grey and squares get pushed towards the top. 2)cannot draw on resized grid

--- scripts.js
+++ scripts.js
@@ -1,22 +1,31 @@
-const main = document.querySelector("#grid");
+const grid = document.querySelector("#grid");
+
+const initialGridSize = 16;
+
 
 function createGrid(size) {
   for (let i = 0; i < size * size; i++) {
     const div = document.createElement("div");
-    main.appendChild(div);
+    grid.appendChild(div);
     addBoxClass(div)
   }
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+  grid.style.gridTemplateColumns =`repeat(${size}, 1fr)`
+  grid.style.width = "500px";
+  grid.style.height = "500px";
 }
+
+createGrid(initialGridSize);
+
 function addBoxClass(element) {
   element.classList.add("box");
-  const box = document.querySelector(".box");
 }
+
 function testBoxClass(element) {
   return element.classList.includes(".box")
 }
 
-const initialGridSize = 16
-createGrid(initialGridSize);
+
 
 
 let allBoxes = document.querySelectorAll(".box");
@@ -72,7 +81,7 @@ let testing2 = 77;
 // grid.classList.add(".resize")
 const resizeButton = document.querySelector("#newGridSize");
 // const resizeClass = document.querySelector(".resize")
-
+let currentGridSize = initialGridSize
 
 resizeButton.addEventListener("click", function () {
   const newSize = prompt('Enter a number below 100');
@@ -80,12 +89,14 @@ resizeButton.addEventListener("click", function () {
 
 })
 
+// const grid = document.querySelector("#grid")
+
 function resizeGrid(size) {
   removeGrid();
   createGrid(size);
-}
+  currentGridSize = size;
 
-let currentGridSize = initialGridSize
+}
 
 function removeGrid() {
   allBoxes.forEach(function (box) {
