e01291127aee6258ab8886d9adb4eead168413a9
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-03-06 15:22:39 -0500

    put everything in the center

--- scripts.js
+++ scripts.js
@@ -1,58 +1,51 @@
-// ORIGINAL GRID
-const main = document.querySelector("#grid")
+const main = document.querySelector("#grid");
 
 function createGrid(size) {
-    for (let i = 0; i < (size * size); i++) {
-        const div = document.createElement("div")
-        main.appendChild(div)
-        div.classList.add("box")
-        const box = document.querySelector(".box")
-        }
-    }
-
-createGrid(16)    
-
-// FILL BOXES: ----------------------------------------
-// BLACK AND WHITE
-const allBoxes = document.querySelectorAll(".box")
-function activateMouseOver(color) {
-    allBoxes.forEach(function(b) {
-    b.addEventListener("mouseover", function() {
-        b.style.backgroundColor = color
-    })
-})}
-activateMouseOver("black")
-// RANDOM COLORS--------------------------------------
-
-// ERASER -------------------------------------------------
-
-// CLEAR GRID -----------------------------------------
-const clear = document.querySelector("#clear")
-function clearBoxes(){
-allBoxes.forEach(function(b) {
-    b.style.backgroundColor = "white"
-})
-}
-clear.addEventListener("click", clearBoxes)
-    
-
-// CHANGE GRID SIZE----------------------------------------------
-const changeGridSize = document.querySelector("#newGridSize")
-changeGridSize.addEventListener("click", function() {
-    let newSize = prompt("Enter desired grid size (must be 100 or less)")
-    if(validateGridSize(newSize)){
-        allBoxes.remove()
-        createGrid(newSize)
-        adjustGridCSS(grid, newSize)
-    }
-})
-
-function validateGridSize(size){
-    return size <= 100
+  for (let i = 0; i < size * size; i++) {
+    const div = document.createElement("div");
+    main.appendChild(div);
+    div.classList.add("box");
+    const box = document.querySelector(".box");
+  }
 }
 
-function adjustGridCSS(element, size) {
-    element.style.gridTemplateColumns = size
-    element.style.gridTemplateRows = size
+createGrid(16);
+
+const allBoxes = document.querySelectorAll(".box");
+allBoxes.forEach(function (b) {
+  b.addEventListener("mouseover", function () {
+    b.style.backgroundColor = "black";
+  });
+});
+
+const clear = document.querySelector("#clear");
+
+function clearBoxes() {
+  allBoxes.forEach(function (b) {
+    b.style.backgroundColor = "white";
+  });
+    eraser.classList.remove(".clicked")
 }
 
+clear.addEventListener("click", clearBoxes);
+
+// ERASER
+const eraser = document.querySelector("#eraser");
+eraser.addEventListener("click", function () {
+  eraser.classList.toggle(".clicked");
+  console.log(eraser.classList.contains(".clicked"));
+  erase();
+});
+
+function erase() {
+  allBoxes.forEach(function (b) {
+      b.addEventListener("mouseover", function () {
+            if (eraser.classList.contains(".clicked")) {
+        b.style.backgroundColor = "white";
+            }
+            else {
+        b.style.backgroundColor = "black"
+            }
+      });
+  });
+}
